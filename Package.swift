d19d40fb6c9e4cb548d0287c23983fe5c2bf0e5d
David Laubenstein <david.laubenstein@posteo.de>
2019-07-22 16:36:10 +0200

    update version to 11

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SimpleKeychain",
-    // platforms: [.iOS("8.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
+    // platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
     products: [
         .library(name: "SimpleKeychain", targets: ["SimpleKeychain"])
     ],
